75e4867307e6940ed3aa5230c2a9752989bdd417
Daniel Merigo <merigodaniel@outlook.com>
2022-11-07 20:23:29 -0300

    Add homework javascript

---
+++ script.js
@@ -0,0 +1,69 @@
+/* 
+const person = {
+    children: [
+        {
+            name: "Daniel",
+            sex: "male"
+        },
+        {
+            name: "Joana",
+            sex: "Female"
+        },
+        {
+            name: "Juliana",
+            sex: "Female"
+        },
+        {
+            name: "Alex",
+            sex: "male"
+        },
+        {
+            name: "Ana",
+            sex: "Female"
+        },
+        {
+            name: "Claudia",
+            sex: "Female"
+        }
+    ]
+}
+
+    for(let i = 0; i < person.children.length; i++){
+        if(person.children[i].sex == "Female" || person.children[i].sex == "female"){
+            console.log(person.children[i].name)
+        }
+    }
+
+
+*/
+
+/** 
+const array1 = [11, 6, 7, 8, 9, 15];
+const array2 = []
+
+function inverteArray() {
+    for (let i = 0; i < array1.length; i++) {
+        array2[i] = array1[(array1.length-i) -1];
+    }
+    console.log(`Os números da Array eram :${array1}\n`)
+    console.log(`E a ordem dos valores ao contrario fica: ${array2}`)
+}
+inverteArray()
+*/
+
+// const numero = 12345;
+// var resultado = "";
+
+// const string = (numero).toString()
+// var lista = string.split("")
+
+// function inverteNumero(){
+// for (let i = 0; i < lista.length; i++) {
+//     resultado = lista[i] + resultado;
+    
+// }
+// console.log(`O Número ao contrario é: `, resultado)
+// }
+// inverteNumero()
+
+
